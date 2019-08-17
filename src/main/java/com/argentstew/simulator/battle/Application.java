7aa3703c10b8615e689e9faab0102136bc719df7
Craig <craig.stewart12@gmail.com>
2019-08-17 15:07:09 -0400

    Added application framework

---
+++ src/main/java/com/argentstew/simulator/battle/Application.java
@@ -0,0 +1,17 @@
+package com.argentstew.simulator.battle;
+
+/**
+ * com.argentstew.simulator.battle
+ * 8/17/2019
+ *
+ * @author Craig
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Fighter 1 enters!");
+        System.out.println("Fighter 2 enters!");
+        System.out.println("FIGHT!");
+        System.out.println("Fighter 1 wins!");
+    }
+}
