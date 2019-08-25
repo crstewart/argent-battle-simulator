c38d84b0b27347dfa4a528fbbc4dd855f14ed471
Craig <craig.stewart12@gmail.com>
2019-08-25 01:24:31 -0400

    Balance changes for Mario

--- src/main/java/com/argentstew/simulator/battle/vg/factory/VgFighterFactory.java
+++ src/main/java/com/argentstew/simulator/battle/vg/factory/VgFighterFactory.java
@@ -35,9 +35,9 @@ public class VgFighterFactory {
                 defenses.set(AttackType.MAGIC, 0.9);
                 defenses.set(AttackSubType.PIERCE, 1.2);
                 defenses.set(AttackSubType.FIRE, 1.2);
-                stats = FighterStats.builder().strength(6.5).toughness(5.5).dexterity(5.5).aim(6)
+                stats = FighterStats.builder().strength(6).toughness(5.5).dexterity(5.5).aim(6)
                         .agility(5).speed(5).intellect(5).willpower(5.5).size(4).weight(4.5).build();
-                traits = new FighterTraits(Collections.singletonList(new Rival("Bowser", 1.125)));
+                traits = new FighterTraits(Collections.singletonList(new Rival("Bowser", 1.1)));
                 return Fighter.builder().name("Mario").hp(150).maxHp(150).xStrikeMeter(0)
                         .entryQuotes(Arrays.asList("Mario: 'Here we go!'", "Mario: 'It's-a me, Mario!'"))
                         .victoryQuotes(Arrays.asList("Mario: 'Thank you so much for playing my game!'",
