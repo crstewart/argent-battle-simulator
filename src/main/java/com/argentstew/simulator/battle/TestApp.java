4cea7eb58182e643981409e25a6f4f4ad6397fa7
Craig <craig.stewart12@gmail.com>
2020-12-24 21:06:16 -0500

    Balance changes for Fox

--- src/main/java/com/argentstew/simulator/battle/TestApp.java
+++ src/main/java/com/argentstew/simulator/battle/TestApp.java
@@ -139,7 +139,7 @@ import java.util.List;
  */
 public class TestApp {
 
-    private static final String TEST_FIGHTER = Ezio.NAME;
+    private static final String TEST_FIGHTER = Fox.NAME;
 
     public static void main(String[] args) {
 
--- src/main/java/com/argentstew/simulator/battle/vg/fox/Fox.java
+++ src/main/java/com/argentstew/simulator/battle/vg/fox/Fox.java
@@ -34,7 +34,7 @@ public class Fox {
         FighterStats stats = FighterStats.builder().strength(4.5).toughness(5.5).dexterity(8.5).aim(8.5)
                 .agility(6.5).speed(6.5).intellect(6).willpower(6).size(4.5).weight(4.5).build();
         FighterTraits traits = new FighterTraits(Arrays.asList(new StealthDetection(), new Rival(Wolf.NAME, 1.3)));
-        return Fighter.builder().name(NAME).hp(125).maxHp(125).xStrikeMeter(0)
+        return Fighter.builder().name(NAME).hp(135).maxHp(135).xStrikeMeter(0)
                 .description("A weaponmaster with a reflective guard and a blaster that does not stun opponents.")
                 .entryQuotes(Arrays.asList("Fox: 'Here I come!'", "Fox: 'Star Fox, ready for duty!'"))
                 .victoryQuotes(Arrays.asList("Fox: 'This is Fox, returning to base!'",
